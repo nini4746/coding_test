613bfa0e0340e058e7632b65d4b880c41df3ed71
nini4746 <165309414+nini4746@users.noreply.github.com>
2025-07-01 12:10:22 +0900

    3-1 문제 추가

---
+++ 3장/1.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[][] line1 = {{2, -1, 4}, {-2, -1, 4}, {0, -1, 1}, {5, -8, -12}, {5, 8, 12}};
+        int[][] line2 = {{0, 1, -1}, {1, 0, -1}, {1, 0, 1}};
+        int[][] line3 = {{1, -1, 0}, {2, -1, 0}};
+        int[][] line4 = {{1, -1, 0}, {2, -1, 0}, {4, -1, 0}};
+    }
+}
+
+class line {
+    private final int x;
+    private final int y;
+    private final int a;
+
+    public line(int x, int y, int a) {
+        this.x = x;
+        this.y = y;
+        this.a = a;
+    }
+
+    public ArrayList<Integer> getIntersection(line line1, line line2) {
+        ArrayList<Integer> intersection = new ArrayList<>();
+
+    }
+}
+
+
+class Solution {
+    public String[] solution(int[][] line) {
+        String[] answer;
+
+        for(int i = 0; i<line.length; i++) {
+
+        }
+
+
+        return answer;
+    }
+}
